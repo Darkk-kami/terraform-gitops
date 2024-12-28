218ab1bbcbb7b0392d7584279010a476d9cc2fda
kami <raw.dwayne@gmail.com>
2024-12-28 16:41:50 +0100

    test

---
+++ backend/prestart.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import subprocess
+
+def run_command(command):
+    try:
+        subprocess.run(command, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
+        exit(1)
+
+# Initialization tasks
+print("Initializing service...")
+run_command(["python", "./app/backend_pre_start.py"])
+run_command(["alembic", "upgrade", "head"])
+run_command(["python", "./app/initial_data.py"])
+
+# Start uvicorn
+print("Starting the application with uvicorn...")
+subprocess.run(["poetry", "run", "uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"], check=True)
